954c0799e02375f0beeae550c3b4352ea440a89f
rafael.lumang <lumang.rafael@gmail.com>
2022-06-10 11:37:14 -0700

    Add downloaded project files

--- random-quotes-generator.js
+++ random-quotes-generator.js
@@ -8,10 +8,6 @@ const quotes = [
         queen: "Shangela"
     },
     { 
-        quote: "Go back to Party City where you belong!",
-        queen: "Phi Phi O'Hara"
-    },
-    { 
         quote: "Miss Vanjie.. Miss Vanjie... Miss Vaaaanjiee.....",
         queen: "Vanessa Vanjie Mateo"
     },
@@ -20,12 +16,8 @@ const quotes = [
         queen: "Bianca Del Rio"
     },
     { 
-        quote: "Look how orange you look, girl!",
-        queen: "Alyssa Edwards"
-    },
-    { 
         quote: "HIIIIIEEEEEEEEEE!!!!",
-        queen: "Alaska Thunderfuck 5000"
+        queen: "Alaska 5000"
     },
     { 
         quote: "This is not Rupaul's Best Friend Race!",
@@ -36,16 +28,8 @@ const quotes = [
         queen: "Adore Delano"
     },
     { 
-        quote: "This is not Rupaul's Best Friend Race!",
-        queen: "Lashauwn Beyond"
-    },
-    { 
         quote: "Well guess what Mimi? We did",
         queen: "Raven"
-    },
-    { 
-        quote: "I feel very attacked right now!",
-        queen: "Laganja Estranga"
     },
     { 
         quote: "You're perfect, you're beautiful, you look like Linda Evangelista, you're a model! Everything about you is perfect. Did you stone those tights? Oh, you're smiling! They eat her up every single time she's on that damn stage!",
@@ -55,4 +39,4 @@ const quotes = [
         quote: "Look over there!",
         queen: "Jaida Essence Hall"
     }
-]
+];
