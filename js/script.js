c7b9c8e80319972481fc014985755e55028258a0
rafael.lumang <lumang.rafael@gmail.com>
2022-10-22 13:46:28 -0700

    Moved css, html, and js files

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@ function getRandomQuote (arr) {
  */
 
 function random_bg_color() {
-   let x = Math.floor(Math.random() * 256);
+    let x = Math.floor(Math.random() * 256);
     let y = Math.floor(Math.random() * 256);
     let z = Math.floor(Math.random() * 256);
     let bgColor = "rgb(" + x + "," + y + "," + z + ")";
